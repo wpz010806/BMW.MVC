6933c62512f3745025c97e331b58bcc7d8cbdc25
谢震 <46702380+hardshells@users.noreply.github.com>
2020-09-17 15:51:07 +0800

    1111

--- CarBMW.MVC/CarBMW.MVC/CarBMW.MVC/CarBMW.MVC/Models/ErrorViewModel.cs
+++ CarBMW.MVC/CarBMW.MVC/CarBMW.MVC/CarBMW.MVC/Models/ErrorViewModel.cs
@@ -4,7 +4,6 @@ namespace CarBMW.MVC.Models
 {
     public class ErrorViewModel
     {
-        int a = 0;
         public string RequestId { get; set; }
             
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
